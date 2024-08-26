4d84038efcec05e8929c5fe10b9d2679b05999d4
yashwanth <Yashwanth@chegus.com>
2024-08-26 10:28:06 +0530

    cdk scroll optimized

--- src/app/cdk-virtual-scroll/cdk-virtual-scroll.component.ts
+++ src/app/cdk-virtual-scroll/cdk-virtual-scroll.component.ts
@@ -16,7 +16,7 @@ export class CdkVirtualScrollComponent implements OnInit, AfterViewInit {
   totalData: any = [];
   scrollable: Subscription;
   isLoading: boolean = false;
-
+  isRedered: boolean = false;
   constructor(
     private bankServ: BankService
   ) { }
@@ -25,66 +25,67 @@ export class CdkVirtualScrollComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {
     this.firstHitApi();
   }
-  
+
   ngAfterViewInit(): void {
     if (this.viewport) {
       this.scrollable = this.viewport.scrolledIndexChange.subscribe(index => {
-        
         // console.log(this.viewport.getDataLength(),'data length');
         // console.log(this.viewport.getRenderedRange(),'data rendered range');
         if (index >= this.dataList.length - 2 && this.isLoading) {
-
           this.nextApiHit();
-        } else if (index <= 1 && this.isLoading && this.start > 0) {
+        } else if (this.isRedered && index <= 1 && this.isLoading && this.start > 0) {
           this.prevDataLoad();
+          // console.log('index',index);
+          // this.dataList = [...this.totalData.slice(-this.count),...this.dataList];
+          // this.totalData.splice(-this.count)
+          // console.log(this.dataList);
         }
       });
     }
   }
-  
+
 
   firstHitApi() {
-    
+    this.isRedered = true;
     this.bankServ.getAllPersons(this.start, this.count).subscribe(data => {
-      
-      console.log('hiting after load of view init api');
-      console.log(this.dataList,'after load of view init');
       this.totalData = [...data];
       this.dataList = [...data];
       this.start += this.count;
-      // console.log(this.dataList,'after load of view init');
-
       this.isLoading = true;
     });
   }
-  
+
   nextApiHit() {
+    // console.log('totalData..',this.totalData);
     this.bankServ.getAllPersons(this.start, this.count).subscribe(data => {
-      if (data && data.length > 0) {
-        this.totalData = [...this.totalData, ...data];
-        this.dataList = [...this.dataList.slice(-this.count), ...data];
-        this.start += this.count;
-      }
+      this.totalData = [...this.totalData, ...data];
+      this.dataList = [...this.dataList.slice(-this.count), ...data];
+      this.start += this.count;
     });
+    // console.log('totalData..',this.totalData);
+
   }
-  
+
   prevDataLoad() {
-    console.log('to ge t prev stat',this.start,'checking the thisstart')
-    const prevvStart = this.start - 2*this.count
-    console.log('star of t he previous data',prevvStart);
-    
-    if (prevvStart < 0){
+    console.log('to ge t current stat',this.start,'checking the thisstart')
+    const prevvStart = this.start - 2 * this.count
+    console.log('star of t he previous start',prevvStart);
+    console.log(this.totalData, 'total data -----------------------------------')
+
+
+    if (prevvStart < 0) {
       return;
-    } 
+    }
+    this.totalData.splice(-this.count)
     const prevData = this.totalData.slice(prevvStart, prevvStart + this.count)
     console.log('prev sub srt of data ---',prevData)
 
-    console.log(this.totalData,'total data -----------------------------------')
+    console.log(this.totalData, 'total data -----------------------------------')
     if (prevData.length > 0) {
       this.dataList = [...prevData, ...this.dataList.slice(0, this.count)];
       this.start -= this.count;
     }
-    console.log(this.dataList,'Modify data after removing the dtata which is not in view port -----------------------------------')
+    // console.log(this.dataList,'Modify data after removing the dtata which is not in view port -----------------------------------')
 
   }
 
