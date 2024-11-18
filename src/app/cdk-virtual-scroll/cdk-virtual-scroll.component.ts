00e66cb02e12953621033cf4f5382ea06ad34745
Yashwanth Lakkakula <Yashwanth@chegus.com>
2024-11-18 15:16:49 +0530

    no changes

--- src/app/cdk-virtual-scroll/cdk-virtual-scroll.component.ts
+++ src/app/cdk-virtual-scroll/cdk-virtual-scroll.component.ts
@@ -30,68 +30,133 @@ export class CdkVirtualScrollComponent implements OnInit, AfterViewInit {
 
   @ViewChild(CdkVirtualScrollViewport) viewport: CdkVirtualScrollViewport;
   ngOnInit(): void {
-    this.firstHitApi();
+    // this.firstHitApi();
     this.dropDowns();
-    this.testLst = Array.from({ length: 100 }, (_, index) => index + 1);
   }
 
-  ngAfterViewInit(): void {
-    if (this.viewport) {
-      this.scrollable = this.viewport.scrolledIndexChange.subscribe(index => {
-        if (index >= this.dataList.length - 2 && this.isLoading) {
-          this.nextApiHit();
-        } else if (this.isRedered && index <= 1 && this.isLoading && this.start > 0) {
-          this.prevDataLoad();
-        }
-      });
-    }
-  }
-  firstHitApi() {
-    this.isRedered = true;
-    this.isLoading = true;
-    this.bankServ.getAllPersons(this.start, this.count).subscribe(data => {
-      this.totalData = data;
-      this.dataList = data;
-      this.start += this.count;
-    });
-  }
-  nextApiHit() {
-    this.bankServ.getAllPersons(this.start, this.count).subscribe(data => {
-      this.totalData = [...this.totalData, ...data];
-      this.dataList = [...this.dataList.slice(-this.count), ...data];
-      this.start += this.count;
-    });
-  }
-  prevDataLoad() {
-    console.log('to ge t current stat', this.start, 'checking the thisstart')
-    const prevvStart = this.start - 2 * this.count
-    if (prevvStart < 0) {
-      return;
-    }
-    this.totalData.splice(-this.count)
-    const prevData = this.totalData.slice(prevvStart, prevvStart + this.count)
-    if (prevData.length > 0) {
-      this.dataList = [...prevData, ...this.dataList.slice(0, this.count)];
-      this.start -= this.count;
-    }
-  }
+  // ngAfterViewInit(): void {
+  //   if (this.viewport) {
+  //     this.scrollable = this.viewport.scrolledIndexChange.subscribe(index => {
+  //       if (index >= this.dataList.length - 2 && this.isLoading) {
+  //         this.nextApiHit();
+  //       } else if (this.isRedered && index <= 1 && this.isLoading && this.start > 0) {
+  //         this.prevDataLoad();
+  //       }
+  //     });
+  //   }
+  // }
+  // firstHitApi() {
+  //   this.isRedered = true;
+  //   this.isLoading = true;
+  //   this.bankServ.getAllPersons(this.start, this.count).subscribe(data => {
+  //     this.totalData = data;
+  //     this.dataList = data;
+  //     this.start += this.count;
+  //   });
+  // }
+  // nextApiHit() {
+  //   this.bankServ.getAllPersons(this.start, this.count).subscribe(data => {
+  //     this.totalData = [...this.totalData, ...data];
+  //     this.dataList = [...this.dataList.slice(-this.count), ...data];
+  //     this.start += this.count;
+  //   });
+  // }
+  // prevDataLoad() {
+  //   console.log('to ge t current stat', this.start, 'checking the thisstart')
+  //   const prevvStart = this.start - 2 * this.count
+  //   if (prevvStart < 0) {
+  //     return;
+  //   }
+  //   this.totalData.splice(-this.count)
+  //   const prevData = this.totalData.slice(prevvStart, prevvStart + this.count)
+  //   if (prevData.length > 0) {
+  //     this.dataList = [...prevData, ...this.dataList.slice(0, this.count)];
+  //     this.start -= this.count;
+  //   }
+  // }
   /**************************************************************************************************************************************************** */
   //common dropdown implementation
 
   Common = this.fb.group({
     Params: this.fb.group({
-      personId: ['Test'],
+      personName: [''],
+      personId: [''],
     }),
-    className: ['DataPeron'],
+    start: [0],
+    count: [10],
+    className: ['DataPersonSearch'],
   })
+
+  ngAfterViewInit(): void {
+    if (this.viewport) {
+      this.scrollable = this.viewport.scrolledIndexChange.subscribe(index => {
+        if (index >= this.cdkLst.length - 8 && this.isLoading) {
+          this.dropDowns();
+        }
+      });
+    }
+  }
   cdkLst: any[] = []
+  totals: any[] = []
   dropDowns() {
-    this.bankServ.getAllPersons(this.start, this.count).subscribe(data => {
-      this.cdkLst = data;
-    });
-    this.bankServ.commonDropdown(this.Common.value).subscribe(data => {
-    });
+    const start = this.Common.get('start')?.value || 0;
+    const count = this.Common.get('count')?.value || 0;
+    try {
+      this.bankServ.commonDropdown(this.Common.value).subscribe(data => {        
+        if (data.length > 0) {
+          this.cdkLst=[...this.cdkLst, ...data];
+          this.totals = [...this.totals, ...data];
+          this.Common.get('start')?.setValue(start + count);
+          this.isLoading = true;
+        }else{
+          this.isLoading = false
+        }
+      });
+    } catch (error) {
+      console.error('Error fetching dropdown data:', error);
+    }
   }
 
+  filteredList:any[] = [] 
+  onKey(event: any) {
+    if (event) {
+      this.cdkLst = []
+        this.cdkLst = this.totals.map(x => {let fil = x.personName.toLowerCase().includes(event.toLowerCase()); return fil==true ? x : []}).filter(y=> y!='');
+        console.log(this.cdkLst)
+    }
+    else{
+      this.cdkLst = this.totals
+    }
+  }
+  /**
+   * Creates a debounced function that delays invoking the provided
+   * function until after `delay` milliseconds have elapsed since the
+   * last time it was invoked.
+   *
+   * @param cb The function to debounce.
+   * @param delay The debounce time in milliseconds. Defaults to 1000.
+   * @returns A debounced version of the provided function.
+   */
+  debounce(cb: (...args: any[]) => void, delay = 1000) {
+    let timeout: ReturnType<typeof setTimeout>;
+    return (...args: any[]) => {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        cb(...args);
+      }, delay);
+    };
+  }
+
+
+  getHeight(): string {
+    const itemCount = this.cdkLst.length;
+    const itemHeight = 30; 
+    const maxHeight = 220; 
+    const minHeight = 100; 
+  
+    const calculatedHeight = Math.min(maxHeight, Math.max(minHeight, itemCount * itemHeight));
+  
+    return `${calculatedHeight}px`;
+  }
 
 }
